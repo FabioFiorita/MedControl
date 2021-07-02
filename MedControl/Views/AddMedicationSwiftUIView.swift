cc94b50db3fa26d18899a7e497736ff54f1119d2
FabioFiorita <fabiolfp@gmail.com>
2021-07-02 13:52:12 -0300

    Button color change to green when clicking in ContentView

--- MedControl/Views/AddMedicationSwiftUIView.swift
+++ MedControl/Views/AddMedicationSwiftUIView.swift
@@ -101,7 +101,7 @@ struct AddMedicationSwiftUIView: View {
             newMedication.date = date
             newMedication.repeatPeriod = repeatPeriod
             newMedication.notes = notes
-            
+            newMedication.isSelected = false
             newMedication.nextDate = newMedication.date
             
             switch newMedication.repeatPeriod {
